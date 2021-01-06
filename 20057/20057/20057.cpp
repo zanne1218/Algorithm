338f092351683d2078863a98217f9d15bee310ff
sehui <zanne1218@naver.com>
2021-01-06 22:48:42 +0900

    20057마법사상어와토네이도
    first try

---
+++ 20057/20057/20057.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	int n;
+	int **a;
+	int out = 0;
+	scanf("%d", &n);
+	for (int r = 0; r < n; r++) {
+		for (int c = 0; c < n; c++) {
+			scanf("%d ", a[r][c]);
+		}
+	}
+	return 0;
+}
