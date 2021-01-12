27546d0f76028733b894c5e17b5662831a518f70
sehui <zanne1218@naver.com>
2021-01-12 21:45:19 +0900

    4948_modify

--- 4948/4948/4948.cpp
+++ 4948/4948/4948.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-#define SIZE 123456
+#define SIZE 246913
 
 using namespace std;
 
+int isNotPrime[SIZE];
+
 int main() {
-	int isNotPrime[SIZE];
 	for (int i = 2; i * i < SIZE; i++) {
 		if (!isNotPrime[i]) {
 			for (int j = i * i; j < SIZE; j += i) {
@@ -13,16 +14,18 @@ int main() {
 		}
 	}
 	isNotPrime[1] = 1;
+	
 	int n, count;
 	while (true) {
 		count = 0;
-		scanf("%d", &n);
+		cin >> n;
 		if (n == 0) break;
 		for (int i = n + 1; i <= 2 * n; i++) {
 			if (!isNotPrime[i])
 				count++;
 		}
-		printf("%d\n", count);
+		cout << count << endl;
 	}
+	
 	return 0;
 }
