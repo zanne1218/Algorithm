c0875510f0a571caee7ea76eb282204c4b09f715
sehui <zanne1218@naver.com>
2021-01-13 23:59:41 +0900

    2447modify

--- 2447/2447/2447.cpp
+++ 2447/2447/2447.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
-
 using namespace std;
-
-int main() {
-	int n;
-	cin >> n;
-	
-	return 0;
+void star(int i, int j, int num)
+{
+	if ((i / num) % 3 == 1 && (j / num) % 3 == 1) {
+		cout << ' ';
+	}
+	else
+	{
+		if (num / 3 == 0)
+			cout << '*';
+		else
+			star(i, j, num / 3);
+	}
 }
-// 3, 9, 27, 81 ...
-
-void star(int number) {
-	
+int main() {
+	int num;
+	cin >> num;
+	for (int i = 0; i < num; i++)
+	{
+		for (int j = 0; j < num; j++)
+			star(i, j, num);
+		cout << '\n';
+	}
 }
