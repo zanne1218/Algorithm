12f843a64d4a3de5d4b78730703029b437f96e10
sehui <zanne1218@naver.com>
2021-01-18 23:52:46 +0900

    1018체스판다시칠하기

---
+++ 1018/1018/1018.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n, m, cnt1 = 0, cnt2 = 0;
+	cin >> n >> m;
+	char **map = new char *[n];
+	for (int i = 0; i < n; i++) {
+		map[i] = new char[m];
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> map[i][j];
+			if ((i % 2 == 0 && j % 2 == 0 && map[i][j] != 'W') ||
+				(i % 2 == 1 && j % 2 == 0 && map[i][j] != 'B'))
+				cnt1++;
+			if ((i % 2 == 0 && j % 2 == 0 && map[i][j] != 'B') ||
+				(i % 2 == 1 && j % 2 == 0 && map[i][j] != 'W'))
+				cnt2++;
+		}
+	}
+	cout << (cnt1 < cnt2) ? cnt1 : cnt2;
+	for (int i = 0; i < n; i++) {
+		delete[] map[i];
+	}
+	delete[] map;
+	return 0;
+}
