ea0de6bfe698a2ec90d2aed2e02d8c3f78928aea
sehui <zanne1218@naver.com>
2021-01-19 19:22:44 +0900

    1018체스판다시칠하기

--- 1018/1018/1018.cpp
+++ 1018/1018/1018.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 int main() {
-	int n, m, cnt1 = 0, cnt2 = 0;
+	int n, m, cnt, min = 64;
 	cin >> n >> m;
-	char **map = new char *[n];
+	char **map = new char *[n];	
 	for (int i = 0; i < n; i++) {
 		map[i] = new char[m];
 	}
@@ -14,7 +14,7 @@ int main() {
 			cin >> map[i][j];
 		}
 	}
-	string chess_black[8] = {
+	string chess[8] = {
 		"BWBWBWBWBW",
 		"WBWBWBWBWB",
 		"BWBWBWBWBW",
@@ -24,21 +24,20 @@ int main() {
 		"BWBWBWBWBW",
 		"WBWBWBWBWB"
 	};
-	string chess_white[8] = {
-		"WBWBWBWBWB",
-		"BWBWBWBWBW",
-		"WBWBWBWBWB",
-		"BWBWBWBWBW",
-		"WBWBWBWBWB",
-		"BWBWBWBWBW",
-		"WBWBWBWBWB",
-		"BWBWBWBWBW"
-	};
-	for (int i = 0; i < n - 8; i++) {
-		for (int j = 0; j < m - 8; j++) {
-			
+	for (int i = 0; i <= n - 8; i++) {
+		for (int j = 0; j <= m - 8; j++) { // 시작부분
+			cnt = 0;
+			for (int k = i; k < i + 8; k++) {
+				for (int m = j; m < j + 8; m++) {
+					if (map[k][m] != chess[k - i][m - j])
+						cnt++;
+				}
+			}
+			cnt = (cnt < 64 - cnt) ? cnt : 64 - cnt;
+			min = (cnt < min) ? cnt : min;
 		}
 	}
+	cout << min;
 	for (int i = 0; i < n; i++) {
 		delete[] map[i];
 	}
